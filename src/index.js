334c42baaa855329a1bd9f0a60f169b3c42543ff
Awais Amjed <awaisamjed171@gmail.com>
2022-03-01 19:30:29 +0500

    Add Placeholder for Empty Scores Table

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import { scoreFormEventListener } from './modules/html_functions.js';
 
 const kraken = new Kraken();
 
+if (kraken.scores.length === 0) {
+  document.getElementById('scores-table').innerHTML = `
+        <p class="empty-table">No Scores yet.</p>
+      `;
+}
+
 document.getElementById('score-form').addEventListener('submit',(e) => {
   e.preventDefault();
   scoreFormEventListener(kraken);
