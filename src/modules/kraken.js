27d8f327076f2ddd64c3256d20aea2062adc19c4
Awais Amjed <awaisamjed171@gmail.com>
2022-03-02 17:13:30 +0500

    Remove Extra Game ID

--- src/modules/kraken.js
+++ src/modules/kraken.js
@@ -2,7 +2,6 @@ export default class Kraken {
   scores = [];
 
   gameID = 'lXsI44Imhn886rk4uGOx';
-  // gameID = 'Zl4d7IVkemOTTVg2fUdz';
 
   addNewScore = (score) => {
     this.scores.push(score);
